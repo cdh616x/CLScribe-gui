095db0226056a0fb968ae95b92a4c5c5efa1fc7e
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-15 17:34:58 -0500

    able to parse a user's input

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+# FUNCTIONS
+def file_namer():
+    file = user_file_name.get("1.0", END)
+    print(file)
+
+
 # UI SETUP
 window = Tk()
 window.title("CLScribe")
@@ -12,26 +18,38 @@ title_label = Label(text="Cover Letter Scribe", relief="raised")
 title_label.grid(column=0, row=0)
 
 # PROMPTS
+file_name = Label(text="What name would you like to save this file under?")
+file_name.grid(column=0, row=1)
+
 greeting = Label(text="Please provide a warm greeting to begin.")
-greeting.grid(column=0, row=1)
+greeting.grid(column=0, row=2)
 
 opening = Label(text="Write an opening statement to introduce yourself.\nThis should reference the company and position"
                      " being applied for", justify="left")
-opening.grid(column=0, row=2)
+opening.grid(column=0, row=3)
 
 qualifications = Label(text="Now, list your qualifications for the position.\nWhat makes you the ideal candidate?",
                        justify="left")
-qualifications.grid(column=0, row=3)
+qualifications.grid(column=0, row=4)
 
 
 # USER TEXT
+user_file_name = Text(bg="black", fg="white", width=50, height=1)
+user_file_name.grid(column=1, row=1)
+
 greeting_text = Text(bg="black", fg="white", width=50, height=2, wrap="word")
-greeting_text.grid(column=1, row=1)
+greeting_text.grid(column=1, row=2)
 
 opening_text = Text(bg="black", fg="white", width=50, height=4, wrap="word")
-opening_text.grid(column=1, row=2)
+opening_text.grid(column=1, row=3)
 
 
+# BUTTONS
+submit = Button(text="Create Document", command=file_namer)
+submit.grid(column=2, row=0)
 
+
+file_namer()
 
 window.mainloop()
+
