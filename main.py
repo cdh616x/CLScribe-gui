dcf2c545d6c9ce3adedbf97980c414940193a18f
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-15 19:26:25 -0500

    added revert capability to clear all entries at once

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import os
+
 
 # FUNCTIONS
 def file_namer():
@@ -6,12 +8,27 @@ def file_namer():
     greeting = greeting_text.get("1.0", END)
     opening = opening_text.get("1.0", END)
     qualifications = qualifications_text.get("1.0", END)
+    hobbies = hobbies_text.get("1.0", END)
+    conclusion = conclusion_text.get("1.0", END)
+    goodbye = goodbye_text.get("1.0", END)
     with open("./output/" + file, "a") as cover_letter:
         cover_letter.write(greeting + "\n")
         cover_letter.write(opening + "\n")
         cover_letter.write(qualifications + "\n")
+        cover_letter.write(hobbies + "\n")
+        cover_letter.write(conclusion + "\n")
+        cover_letter.write(goodbye)
 
-    print(file)
+
+def restore():
+    user_file_name.delete("1.0", END)
+    greeting_text.delete("1.0", END)
+    opening_text.delete("1.0", END)
+    qualifications_text.delete("1.0", END)
+    hobbies_text.delete("1.0", END)
+    conclusion_text.delete("1.0", END)
+    goodbye_text.delete("1.0", END)
+
 
 
 # UI SETUP
@@ -36,10 +53,21 @@ opening = Label(text="Write an opening statement to introduce yourself.\nThis sh
                      " being applied for", justify="left")
 opening.grid(column=0, row=3)
 
-qualifications = Label(text="Now, list your qualifications for the position.\nWhat makes you the ideal candidate?",
+qualifications = Label(text="Now, list your professional qualifications for the position.\nWhat makes you the ideal "
+                            "candidate?",
                        justify="left")
 qualifications.grid(column=0, row=4)
 
+hobbies = Label(text="What other things would you like the company to know?\nOther resources, hobbies, anything else "
+                     "that would make you a better candidate...")
+hobbies.grid(column=0, row=5)
+
+conclusion = Label(text="Add a conclusion paragraph hitting all the main points again.\nInclude a few forms of "
+                        "contact information and express a desire to speak in person")
+conclusion.grid(column=0, row=6)
+
+goodbye = Label(text="End the letter with a goodbye (Sincerely, etc.)\nStretch over two lines!")
+goodbye.grid(column=0, row=7)
 
 # USER TEXT
 user_file_name = Text(bg="black", fg="white", width=50, height=1)
@@ -51,16 +79,23 @@ greeting_text.grid(column=1, row=2)
 opening_text = Text(bg="black", fg="white", width=50, height=3, wrap="word")
 opening_text.grid(column=1, row=3)
 
-qualifications_text = Text(bg="black", fg="white", width=50, height=5, wrap="word")
+qualifications_text = Text(bg="black", fg="white", width=50, height=8, wrap="word")
 qualifications_text.grid(column=1, row=4)
 
+hobbies_text = Text(bg="black", fg="white", width=50, height=8, wrap="word")
+hobbies_text.grid(column=1, row=5)
+
+conclusion_text = Text(bg="black", fg="white", width=50, height=4, wrap="word")
+conclusion_text.grid(column=1, row=6)
+
+goodbye_text = Text(bg="black", fg="white", width=50, height=2)
+goodbye_text.grid(column=1, row=7)
 
 # BUTTONS
 submit = Button(text="Create Document", command=file_namer)
 submit.grid(column=1, row=0)
 
-
-file_namer()
+restore = Button(text="Restore", command=restore)
+restore.grid(column=1, row=8)
 
 window.mainloop()
-
